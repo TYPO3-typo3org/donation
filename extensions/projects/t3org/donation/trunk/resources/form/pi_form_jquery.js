87f4744e2cb3b6afe98be6aa6cce4e3b056e91cf
timek <timek@e8cb78a8-64f2-4011-8042-bd1ea5058d47>
2011-10-11 14:13:14 +0000

    fixed monthly subscription

--- extensions/projects/t3org/donation/trunk/resources/form/pi_form_jquery.js
+++ extensions/projects/t3org/donation/trunk/resources/form/pi_form_jquery.js
@@ -1,6 +1,8 @@
 
 jQuery(document).ready(function() {
     dform = $('.tx-donation-pi-form form.paypal');
+    $('form.paypal div.paypal-checkboxes').prepend('<div class="b-form-row"><label>One Time Donation</label><div class="b-form-inputs"><input type="radio" name="donationtype" value="onetype"/></div></div>');
+
     dform.each(function(){
 
 	$('input[name="donationtype"]',dform).change(function(){
@@ -9,7 +11,7 @@ jQuery(document).ready(function() {
 		$('input[name="cmd"]').val('_donations');
 		$('input[name="bn"]').val('PP-DonationsBF');
 		$('input[name="item_name"]',dform).val('TYPO3 One-Time Donation');
-
+		$('input[name="a3"]',dform).attr('name','amount');
 	    }
 	    if($(this).val()=='subscription'){
 		$('div.additional',dform).html(
@@ -18,6 +20,8 @@ jQuery(document).ready(function() {
 	+'<input type="hidden" name="src" value="1" />'
 	+'<input type="hidden" name="sra" value="1" />'
 	);   
+	    paypal
+	    $('input[name="amount"]',dform).attr('name','a3');
 	    $('input[name="cmd"]',dform).val('_xclick-subscriptions');
 	    $('input[name="bn"]',dform).val('PP-SubscriptionsBF');
 	    $('input[name="item_name"]',dform).val('TYPO3 Recuring Donations');
